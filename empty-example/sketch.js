ea3a5cbe8fb6d27db891547ba7fe87cc92f203f9
MetteZeuner <mette.zeuner@outlook.dk>
2017-03-19 21:50:25 +0100

    Update sketch.js

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -15,11 +15,11 @@ var x = random(0, 600);
 var y = random(0, 400);
 
     
-if(circle1 < 10 || circle1 > 20){
+if(circle1 < 10 || circle1 > 20){ //rule 1 - makes the circles a different size
   goback = goback * -1;   
 }
 
-if(circle1 < 15){
+if(circle1 < 15){ //rule 2 - changes the colours of the circles between black and white. 
     fill(0);
 }else{
     fill(255);
@@ -29,19 +29,14 @@ stroke(0);
 ellipse(x, y, circle1, circle1);
     circle1 = circle1 + goback; 
 
-if(circle1 < 11){
-stroke(r, g, b);
-}
 
-if(circle1 < 14){
-stroke(r, g, b);
-}
+stroke(r, g, b); //changes the color of the lines to a different color everytime you run the program. 
+
     
-if(circle1 < 13){   
+if(circle1 < 13){ //rule 3 - connects a line to a cricle that is smaller than 13. 
 line(x, y, 300, 200);
 }
     
     
 }
 
-//85, 75
